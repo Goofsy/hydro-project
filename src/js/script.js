264dce6603d1d8a50c33238b5538ce9cce949320
goofsy <dawid.stud@gmail.com>
2020-12-10 00:52:50 +0100

    slider-dots

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+// const slides = document.querySelectorAll('.slider__slide');
+// const curSlide = 0;
+
+// slides.forEach((slide, i) => {
+//   slide.style.transform = 'translate(-100%)';
+//   console.log(slide);
+// });
