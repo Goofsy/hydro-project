b5ab7ebca91c470e94c76cc8526874acd39ffb6a
goofsy <dawid.stud@gmail.com>
2020-12-14 00:56:43 +0100

    form-validation

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,6 @@
+import 'core-js';
+import 'regenerator-runtime/runtime';
+
 class Slider {
   _slides = document.querySelectorAll('.slider__slide');
   _btnPrev = document.querySelector('.slider__btn--prev');
@@ -85,17 +88,112 @@ new Slider();
 // Smooth scrolling
 const scroll = new SmoothScroll('.header a[href*="#"]');
   
-// Sending Email
 
-const sendEmail = function() {
-  Email.send({
-    SecureToken : "1b17ee09-4190-4d7c-bd83-efeb38674305",   
-    To : 'dawid.stud@gmail.com',
-    From : "goofson555@gmail.com",
-    Subject : "This is the subject",
-    Body : "And this is the body"
-  }).then(
-  message => alert(message)
-  );
+class SendEmail {
+  _form = document.querySelector('.form');
+  _formInfo = document.querySelector('.form__info');
+  _inputName = document.querySelector('#name');
+  _inputEmail = document.querySelector('#email');
+  _inputPhone = document.querySelector('#phone');
+  _inputMessage = document.querySelector('#message');
+  
+  constructor() {
+    this._addHandlerForm();
+    this._addFilledClass();
+    this._removeErrorClassHandler();
+  }
+
+  _removeErrorClassHandler() {
+    this._form.addEventListener('click', this._removeErrorClass.bind(this));
+  }
+
+  _removeErrorClass(e) {
+    if (!e.target.closest('.form__group__input--error')) return;
+    e.target.classList.remove('form__group__input--error');
+    this._formInfo.innerHTML = '';
+    this._formInfo.classList.remove('form__info--error');
+  }
+
+  _addFilledClass() {
+    this._form.addEventListener('change', e => {
+      if (e.target.value.length === 0 || e.target.value === '')  e.target.classList.remove('form__group__input--filled');
+      else {
+        e.target.classList.add('form__group__input--filled');
+      }
+    });
+  }
+
+  _removeFilledClass() {
+    document.querySelectorAll('.form__group__input').forEach(input => {
+      input.classList.remove('form__group__input--filled');
+    });
+  }
+
+  _addHandlerForm() {
+    this._form.addEventListener('submit', this._handleForm.bind(this));
+  }
+
+  async _handleForm(e) {
+    e.preventDefault();
+    try {
+      const _inputNameValue = this._inputName.value;
+      const _inputEmailValue = this._inputEmail.value;
+      const _inputPhoneValue = this._inputPhone.value;
+      const _inputMessageValue = this._inputMessage.value;
+
+      if (_inputNameValue.length < 3 || _inputNameValue === null || _inputNameValue === ''){
+        this._inputName.classList.add('form__group__input--error');
+        throw new Error('Imię musi zawierac przynajmniej 3 znaki!');
+      }
+
+      if (!this._validateEmailAddress(_inputEmailValue)) {
+        this._inputEmail.classList.add('form__group__input--error');
+        throw new Error('Niepoprawny e-mail!');
+      }
+
+      if (_inputPhoneValue.length < 8 || _inputPhoneValue === null || _inputPhoneValue === ''){
+        this._inputPhone.classList.add('form__group__input--error');
+        throw new Error('Nr telefonu musi zawierac przynajmniej 8 znaków!');
+      }
+
+      if (_inputMessageValue === null || _inputMessageValue === ''){
+        this._inputMessage.classList.add('form__group__input--error');
+        throw new Error('Napisz coś :)');
+      }
+
+
+      const send = await this._sendEmail(_inputNameValue, _inputEmailValue, _inputPhoneValue, _inputMessageValue);
+      
+      if (!send === 'OK') throw new Error('Coś poszło nie tak.');
+      this._form.reset();
+      this._removeFilledClass();
+      this._formInfo.innerHTML = 'Wiadomośc została wysłana';
+
+    } catch (err) {
+      this._formInfo.innerHTML = err.message;
+      this._formInfo.classList.add('form__info--error');
+    }
+  }
+
+  _validateEmailAddress(email) {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+
+  _sendEmail(name, email, phone, message) {
+      try {
+        return Email.send({
+          // SecureToken : "1b17ee09-4190-4d7c-bsd83-efeb38674305",   
+          SecureToken : "1b17ee09-4190-4d7c-bd83-efeb38674305",   
+          To : 'dawid.stud@gmail.com',
+          From : email,
+          Subject : `${name}, tel: ${phone}`,
+          Body : message
+        }) 
+      } catch (err) {
+        console.log(err);
+      }   
+    }
+
 }
-
+new SendEmail();
